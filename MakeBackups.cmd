332100743404aa1715c5254528437ec616aab5ab
Phillip Donate <272286135+phillipdonate@users.noreply.github.com>
2026-02-20 17:09:11 -0800

    Fix WipeStaging label

--- MakeBackups.cmd
+++ MakeBackups.cmd
@@ -9,7 +9,7 @@ if not exist %MB_DESTINATION% (
     goto :Error
 )
 
-call :WipeMB_STAGING
+call :WipeStaging
 mkdir "%MB_STAGING%" 2>nul
 if errorlevel 1 (
     echo Could not create temporary %MB_STAGING% folder.
@@ -50,7 +50,7 @@ for %%F in ("%MB_DESTINATION%\*.zip" "%MB_DESTINATION%\*.7z") do (
 if "%TEST_ARCHIVE_FAIL%"=="1" goto :Error
 
 :Success
-call :WipeMB_STAGING
+call :WipeStaging
 call :BeepSuccess
 echo.
 echo All done!  Press any key or wait 10 seconds...
@@ -58,7 +58,7 @@ timeout /t 10 >nul
 exit /b 0
 
 :Error
-call :WipeMB_STAGING
+call :WipeStaging
 call :BeepError
 echo.
 echo Something went wrong.  Check errors above!
@@ -74,6 +74,6 @@ exit /b
 powershell -c "[console]::beep(500,120); [console]::beep(350,150); [console]::beep(250,180)"
 exit /b
 
-:WipeMB_STAGING
+:WipeStaging
 rmdir /s /q "%MB_STAGING%" 2>nul
 exit /b
