d89e4a1d8ab7def6477d1c17eac18dc2d0896db4
Phillip Donate <272286135+phillipdonate@users.noreply.github.com>
2026-02-22 14:38:48 -0800

    Allow for archiving directly to destination, skipping staging

--- MakeBackups.cmd
+++ MakeBackups.cmd
@@ -5,31 +5,33 @@ set zipper=%~dp0\7za.exe
 call MakeBackupsConfig.cmd
 
 if not exist %MB_DESTINATION% (
-    echo Could not find destination folder.  Do you need to connect the USB drive?
+    echo Could not find destination folder.
     goto :Error
 )
 
-call :WipeStaging
-mkdir "%MB_STAGING%" 2>nul
-if errorlevel 1 (
-    echo Could not create temporary %MB_STAGING% folder.
-    goto :Error
+set MB_MAKE_STAGING=:Noop
+set MB_MOVE_STAGING=:Noop
+set MB_WIPE_STAGING=:Noop
+
+if "%MB_STAGING%"=="" (
+    set MB_STAGING=%MB_DESTINATION%
+) else (
+    set MB_MAKE_STAGING=:MakeStaging
+    set MB_MOVE_STAGING=:MoveStaging
+    set MB_WIPE_STAGING=:WipeStaging
 )
+
+call %MB_WIPE_STAGING%
+call %MB_MAKE_STAGING%
+if errorlevel 1 goto :Error
 
 for %%S in (%MB_SOURCES%) do (
     call MakeBackupArchive.cmd %%S "%MB_STAGING%"
     if errorlevel 1 goto :Error
 )
 
-echo.
-echo Moving backups to %MB_DESTINATION% ...
-echo.
-
-for %%F in ("%MB_STAGING%\*") do (
-    echo %%~nxF
-    move /Y "%%F" "%MB_DESTINATION%" >nul
-    if errorlevel 1 goto :Error   
-)
+call %MB_MOVE_STAGING%
+if errorlevel 1 goto :Error
 
 echo.
 echo Testing all backups at %MB_DESTINATION% ...
@@ -50,15 +52,15 @@ for %%F in ("%MB_DESTINATION%\*.zip" "%MB_DESTINATION%\*.7z") do (
 if "%TEST_ARCHIVE_FAIL%"=="1" goto :Error
 
 :Success
-call :WipeStaging
+call %MB_WIPE_STAGING%
 call :BeepSuccess
 echo.
-echo All done!  Press any key or wait 10 seconds...
-timeout /t 10 >nul
+echo All done!
+timeout /t 5 >nul
 exit /b 0
 
 :Error
-call :WipeStaging
+call %MB_WIPE_STAGING%
 call :BeepError
 echo.
 echo Something went wrong.  Check errors above!
@@ -76,4 +78,27 @@ exit /b
 
 :WipeStaging
 rmdir /s /q "%MB_STAGING%" 2>nul
+exit /b
+
+:Noop
+exit /b
+
+:MakeStaging
+mkdir "%MB_STAGING%" 2>nul
+if errorlevel 1 (
+    echo Could not create temporary %MB_STAGING% folder.
+    exit /b %errorlevel%
+)
+exit /b
+
+:MoveStaging
+echo.
+echo Moving backups to %MB_DESTINATION% ...
+echo.
+
+for %%F in ("%MB_STAGING%\*") do (
+    echo %%~nxF
+    move /Y "%%F" "%MB_DESTINATION%" >nul
+    if errorlevel 1 exit /b %errorlevel%
+)
 exit /b
--- MakeBackupsConfig.cmd
+++ MakeBackupsConfig.cmd
@@ -1,8 +1,19 @@
 @echo off
+
+rem Archive type to use.  Expecting 7z or zip
 set MB_ARCHIVE_TYPE=7z
+
+rem Destination path for archives
 set MB_DESTINATION=E:\Backups_NameHere
+
+rem Temporary staging path for creating archives before
+rem copying to destination.  Use this if the destination
+rem is on slow or unreliable storage, such as a network
+rem share or USB drive.  Leave unset to skip staging
+
 set MB_STAGING=%TEMP%\Backups_NameHere
 
+rem A list of paths to archive individually
 set MB_SOURCES=^
     "%ONEDRIVE%\KeePass" ^
     "%ONEDRIVE%\Documents"
